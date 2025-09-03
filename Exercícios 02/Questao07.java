4a79acef346e3d17c4d4e52cddbf6fbc3a63a8e9
BrzMatheus <matheuspmenezes2000@gmail.com>
2025-09-02 22:52:04 -0300

    Create Questao07.java

---
+++ Exercícios 02/Questao07.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VerificaNumero {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = entrada.nextInt();
+
+        if (numero > 0) {
+            System.out.println("Positivo");
+        } else if (numero < 0) {
+            System.out.println("Negativo");
+        } else {
+            System.out.println("Nulo");
+        }
+
+        entrada.close();
+    }
+}
